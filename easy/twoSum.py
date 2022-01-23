b739bb14f615ee334ac82bd4151a54b242239d9d
sayo mizoguchi <sl.deve080412@gmail.com>
2022-01-23 11:07:49 -0700

    solved easy problem

---
+++ easy/twoSum.py
@@ -0,0 +1,91 @@
+"""
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+
+
+"""
+
+# This is the solution using Hashmap!
+# 1. make hashmap
+# 2. loop over the nums and we want to return the index so we need to use enumerate
+# 3. make a complemet that target - num
+# 4. if num is not in hashmap, make hashmap table
+# 5. if there is num in hashmap, return num's index and num's index in hasmap
+
+# from _typeshed import SupportsRead
+
+
+def solution(nums, target):
+
+    
+    # hashmap = {}
+    # for index, num in enumerate(nums):
+    #     comp = target - num
+    #     if num not in hashmap:
+    #         print(hashmap)
+    #         hashmap[comp] = num, index
+    #     else:
+    #         return [hashmap[num][1], index]
+    # return -1
+
+
+
+# This is the solution using two pointers
+# 1. we need to sort the nums 
+# 2. set up start = [0] and end = [len(nums):-1]
+# 3. while loop over start to end 
+# 4. we need to make the valiable adding start and end
+
+
+    sortedNums =sorted(nums)
+    # print(sortedNums)
+    start = 0
+    end = len(nums) -1
+    # print(start,end)
+
+
+    while start < end:
+        startandEnd = sortedNums[start] + sortedNums[end]
+        # print(startandEnd)
+        if startandEnd > target:
+            end -= 1
+        if startandEnd < target:
+            start += 1
+        if startandEnd == target:
+            return [start], [end] 
+    return -1
+
+
+
+
+
+print(solution([2,7,11,15],9)) #[0,1]
+print(solution([3,2,4],6))#[0,2]
+print(solution([3,3],6)) #[0,1]
---
+++ easy/validateSubsequence.py
@@ -0,0 +1,45 @@
+"""
+Validate Subsequnce
+
+Given two non-empty arrays of integers, write a function that determines wheather the second array os a subsequence of the first one.
+
+A subsequence of an array is a set of numbers that aren't necessarily adacent in the array but that are in the same order as they apprear in the array. For instance, the numbers [1,3,4] from a subsequence of the array [1, 2, 3, 4], and so do the numbers [2,4]. Note that a single number in an array and the array itself are both valid subsequences of the array.
+
+Sample input
+array = [5,1,22,25,6,-1,8,10]
+sequence = [1,6,-1,10]
+
+Sample output: True
+
+"""
+# This is the solution by using while (two pointer)
+def validareSubsequence(array, sequence):
+    arrayIdx = 0
+    seqIdx = 0
+        # need to check the all of array numbers and sequence numbers
+    while arrayIdx < len(array) and seqIdx < len(sequence):
+        if array[arrayIdx] == sequence[seqIdx]:
+            # if array's number and sequence's number are the same, move tothe next number 
+            seqIdx += 1
+        arrayIdx += 1
+        # why we put arryIdx in outside of if condition? -> regardless of whether or not we have a match, need to check all of array
+    return seqIdx == len(sequence)
+
+# This is the solution by using for loop 
+def validareSubsequenceLoop(array, sequence):
+    seqIdx = 0
+    for value in array:
+        if seqIdx == len(sequence):
+            return True
+        if sequence[seqIdx] == value:
+            seqIdx += 1
+    return seqIdx == len(sequence)
+    # if seqIdx's number and len(sequence) is the same, return True
+
+
+print(validareSubsequence([5,1,22,25,6,-1,8,10],[1,6,-1,10]))
+print(validareSubsequenceLoop([5,1,22,25,6,-1,8,10],[1,6,-1,10]))
+
+
+
+
