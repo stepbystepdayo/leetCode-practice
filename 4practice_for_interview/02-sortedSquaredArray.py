7151b0c190737223ebe38163d644936b8b773e59
sayo mizoguchi <sl.deve080412@gmail.com>
2022-05-11 10:41:50 -0700

    binarysums and review sorting

--- 4practice_for_interview/02-sortedSquaredArray.py
+++ 4practice_for_interview/02-sortedSquaredArray.py
@@ -1,4 +1,4 @@
-'''Write a function that takes in a non-empty array of integrs that are sorted in ascending order and returns a new array of the same length with the squares of yhe original integers also sorted in ascending order'''
+'''Write a function that takes in a non-empty array of integrs that are sorted in ascending order and returns a new array of the same length with the squares of the original integers also sorted in ascending order'''
 
 
 # this is the brute force answer
