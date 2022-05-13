e305b93bf20a946db9020ddf8a909758b6dde5c3
sayo mizoguchi <sl.deve080412@gmail.com>
2022-05-13 11:01:09 -0700

    class photos

---
+++ 4practice_for_interview/16-photoShooting.py
@@ -0,0 +1,25 @@
+'''
+'''
+
+
+def solution(redShirtHeights,blueShirtHeights):
+    redShirtHeights.sort(reverse = True)
+    blueShirtHeights.sort(reverse = True)
+
+    colorFirstRow = 'RED' if redShirtHeights[0] < blueShirtHeights[0] else 'Blue'
+
+    for index in enumerate(redShirtHeights):
+        if colorFirstRow == 'RED':
+            if redShirtHeights >= blueShirtHeights:
+                return False
+        else:
+            if redShirtHeights <= blueShirtHeights:
+                return False
+    return True
+
+
+
+
+print(solution([5,8,1,3,4],[6,9,2,4,5]))
+
+
