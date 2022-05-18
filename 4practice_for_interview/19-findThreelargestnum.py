90e656dc0f3cc18f946d4b9e68decd271aa2649e
sayo mizoguchi <sl.deve080412@gmail.com>
2022-05-18 10:33:13 -0700

    without sorting largest nums

--- 4practice_for_interview/19-findThreelargestnum.py
+++ 4practice_for_interview/19-findThreelargestnum.py
@@ -18,7 +18,26 @@ def solution(array):
     # This is the solution without sorted
 
     # need to make three empty spaces in array 
+    threeLargest = [None, None, None]
+
     
+    for num in array:
+        updateLargest(threeLargest, num)
+    return threeLargest
+
+def updateLargest(threeLargest, num):
+    if threeLargest[2] is None or num > threeLargest[2]:
+        shiftAndUpdate(threeLargest, num,2) 
+    elif threeLargest[1] is None or num > threeLargest[1]:
+        shiftAndUpdate(threeLargest, num,1) 
+    elif threeLargest[0] is None or num > threeLargest[0]:
+        shiftAndUpdate(threeLargest,num,0) 
+def shiftAndUpdate(array, num, index):
+    for i in range(index + 1):
+        if i == index:
+            array[i] = num
+        else:
+            array[i] = array[i + 1]
 
 
 
