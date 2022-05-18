b9439d9a77278c096001650e5b69da8acfca227a
sayo mizoguchi <sl.deve080412@gmail.com>
2022-05-18 11:07:46 -0700

    explination of NO19

--- 4practice_for_interview/19-findThreelargestnum.py
+++ 4practice_for_interview/19-findThreelargestnum.py
@@ -8,31 +8,36 @@
 
 '''
 
-
-def solution(array):
-    #This is the solution using sorted
+ #This is the solution using sorted
+#  def solution(array):
     # sortedArray = sorted(array)
     # split = sortedArray[-3:]
     # return split
 
-    # This is the solution without sorted
 
+# This is the solution without sorted
+def solution(array):
     # need to make three empty spaces in array 
     threeLargest = [None, None, None]
-
-    
     for num in array:
         updateLargest(threeLargest, num)
     return threeLargest
 
+# making the updated largest function 
 def updateLargest(threeLargest, num):
+    # if index[2] is none or number is greater than [2] inside threeLargest array,then we need to use shiftAndUpdate function
     if threeLargest[2] is None or num > threeLargest[2]:
         shiftAndUpdate(threeLargest, num,2) 
+    # if index[1] is none or number is greater than [1] inside threeLargest array,then we need to use shiftAndUpdate function
     elif threeLargest[1] is None or num > threeLargest[1]:
         shiftAndUpdate(threeLargest, num,1) 
+    # if index[0] is none or number is greater than [0] inside threeLargest array,then we need to use shiftAndUpdate function
     elif threeLargest[0] is None or num > threeLargest[0]:
         shiftAndUpdate(threeLargest,num,0) 
+
+# this is for changing the order 
 def shiftAndUpdate(array, num, index):
+    # loop over i and if i is eaqle to index, arrya[i] will be number
     for i in range(index + 1):
         if i == index:
             array[i] = num
