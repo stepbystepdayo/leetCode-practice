b22fcd345d1d00eab90260001c7b842210e90bd3
sayo mizoguchi <sl.deve080412@gmail.com>
2022-05-18 10:32:43 -0700

    find three largest num

---
+++ 5gca/01-dontknowthename.py
@@ -0,0 +1,17 @@
+'''
+example:
+text : "heelloo saayoo" -> string
+sticky = ["e","o","a"]
+
+output = "hello sayo"
+'''
+
+def solution(text, sticky):
+    
+
+
+
+
+
+
+print(solution("heelloo saayoo", ["e","o","a"]))
---
+++ 6review/sorting.py
@@ -0,0 +1,26 @@
+# Insertion Sort
+# 
+# thinking style:
+#  1,3,2,4
+
+# first, we need to compare 1 and 3 then, 3 is bigger than 1 so stay there 
+# second, need to compare 3 and 2 then, 2 is less than 3 so need to swap 2 and 3 => 1,2,3,4
+# third, compare 1 and 2 then, 1 is less than 2. stay there
+# compare 3 and 4 then, 3 is less than 4 
+
+
+# Bubble Sort
+# thinking style:
+# 5,3,1,2,4
+
+# first comprare 5 and 3 then, 5 is bigger than 3 so swap
+# second, compare 5 and 1 then, 5 is bigger than 1 so swap => 3,1,5,2,4
+# compare 5 and 2 then, 5 is bigger so swap => 3,1,2,5,4
+# compare 5 and 4 then, 5 is bigger so swap => 3,1,2,4,5
+# Third, we need to back to first number that mean 3!!
+# then, compare 3 and 1. 3 is bigger so swap => 1,3,2,4,5
+# compare 3 and 2. 3 is bigger so swap => 1,2,3,4,5
+# compare 3 and 4. 4 is bigger than 3 so STOP this loop!! DONE
+
+
+
