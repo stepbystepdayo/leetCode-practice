ce2b06916e5edf5c69c90f3d7d4812315ff765bb
sayo mizoguchi <sl.deve080412@gmail.com>
2022-05-19 11:05:10 -0700

    comment

--- 4practice_for_interview/20-bubbleSorting.py
+++ 4practice_for_interview/20-bubbleSorting.py
@@ -1,22 +1,34 @@
 '''
 
+  If you're unfamiliar with Bubble Sort, we recommend watching the Conceptual
+  Overview section of this question's video explanation before starting to code.
+
+
 '''
 
 
 def solution(array):
     isSorted = False
-    counter = 0
+
     while not isSorted:
         isSorted = True
-        for i in range(len(array) -1 - counter):
+
+        for i in range(len(array) -1):
+            # if the number is greater than the right side of number, swap them.
             if array[i] > array[i+1]:
+                # swap function
                 swap(i,i+1,array)
+                
                 isSorted = False
-        counter += 1
+
     return array
 
 def swap(i,j,array):
+    # array[i] that mean left side of the number will swap to right side of the number. 
     array[i],array[j] = array[j],array[i]
+
+
+
 
 
 
---
+++ 6review/01-linkedlist.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self,value): #Constructor
+        self.value = value
+        self.next = None
+
+head = None
+def insert_at_front(val):
+    pass
+
+insert_at_front(45)
+insert_at_front(88)
+
+
