10cda0a5160f972afa171b89e2f551a904bc8f69
sayo mizoguchi <sl.deve080412@gmail.com>
2022-05-26 09:50:35 -0700

    arthmetic operators

---
+++ 1-algoProgramming/04-arthmeticOperators.py
@@ -0,0 +1,36 @@
+
+# x = 1
+# y = -2.0
+
+# result = x + y
+# # print(result) # 5
+
+# result2 = x - y
+
+
+# xy = 10
+# yx = 3
+
+# result2 = xy // yx # 3 it will chopped off all decimals!
+
+
+s = 11
+k = 2
+z = 4
+
+result3 = s % k  # % is module 11/4 = 2 あまり 3 so return 3 (あまり部分)
+# print(result3)
+
+
+result4 = s % k + 4 - 7 ** (2 / 3)
+
+# ** is ２乗　i.g: 2 ** 2 = 4 | 2 ** 3 = 8  
+
+# 1. brackets / parathensis
+# 2. exponents
+# 3. multiplication and division and mod
+# 4. addtion substraction
+
+# answer = "5" + 6  this is not working ERROR
+# answer = 4.5 / "5"  this is not working ERROR
+# answer = 4.5 / True => 4.5 because True is 1 in python but if 4.5 / False will be ERROR False is 0.
