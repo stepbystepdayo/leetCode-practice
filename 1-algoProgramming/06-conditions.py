df06992a8ad9b34481eaf08c01f391f5a3a857ed
sayo mizoguchi <sl.deve080412@gmail.com>
2022-05-27 10:06:02 -0700

    conditions

---
+++ 1-algoProgramming/06-conditions.py
@@ -0,0 +1,85 @@
+
+# Eaqul and NOT eaqul
+
+# cond = 2 == 3 # False 
+
+# if 2 == 2, return True
+# 3.0 == 3 True
+
+
+# cond2 = 3.0 != 2  # != is not eaqul so True
+
+# x = 4
+# y = 4
+
+# cond3 = x == y
+# print(cond3)
+
+
+# cond4 = x < 9 # x is less than 9 
+# cond5  = y > 2 # y is bigger than 2
+
+# cond6 = x <= 9 # x is less than 9 or eaqul
+# cond7 = y >= 2 # y is bigger than 2 or eaqul
+
+# cond8 = x >= "6" # we cannot conpare integer and string
+
+
+# how to compare string???
+
+# str1 = "Hello"
+# str2 = "hello"
+
+# cond9 = str1 == str2 # False. because str1's H is upper capital and str2's h is lower capital.
+
+# char1 = "a"
+# char2 = "b"
+
+# cond10 = char1 < char2 # True. because this is the correct ascending order
+
+
+# char3 = "ac"
+# char4 = "ab"
+
+# cond11 = char3 <= char4 # False. because when compare c and b, this is not the ascending order so False
+
+
+# '''
+# # each charactors has numbers
+# ASCII
+# "A" - 65
+# "B" - 66
+# '''
+
+# # ord() -> we can call charactor's numbers
+
+print(ord("a")) # 97 
+# print(ord("A")) # 65
+
+# char() -> we can call the charactor from numbers
+
+# print(chr(97)) # "a"
+
+
+chars1 = "ABc"
+chars2 = "ABC"
+
+cond12 = chars1 < chars2 # it will be False. because "C" upper is  67 and "c" lower is 99. upper capital is first so False.
+
+
+cond13 = True == 1 # True
+cond14 = False == 0 # True
+
+cond15 = True == "" or "True" # False
+cond16 = False == "" or "False" # False
+
+
+
+
+
+
+
+
+
+
+
