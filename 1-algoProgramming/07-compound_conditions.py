aeb14b86fedc16f7c4ff4f32fc90a63ddabee040
sayo mizoguchi <sl.deve080412@gmail.com>
2022-05-30 18:49:30 -0700

    compound conditions

---
+++ 1-algoProgramming/07-compound_conditions.py
@@ -0,0 +1,27 @@
+x = 2
+y = 3
+
+# need to be True both between and
+compound = x + y and y + x  > 2
+
+
+# need to be either right or left is true 
+compound2 = x + y or y+x < 2
+
+
+# not True = False then, False == False is True
+compound3 = not True == False # True
+
+'''
+this is a lower precedence
+compound4 = True == not False 
+this is going to be ERROR 
+compound4 = True == (not False) it will run () first 
+
+'''
+
+# DeMogan's Low
+
+not (x and y) == (not x) or *(not y)
+
+
