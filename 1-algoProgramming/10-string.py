c0a32dd4b29de12c9f0877acd89b494deac8d7d8
sayo mizoguchi <sl.deve080412@gmail.com>
2022-05-31 14:52:06 -0700

    string

---
+++ 1-algoProgramming/10-string.py
@@ -0,0 +1,13 @@
+s = "Hello"
+print(s[-1]) # o
+print(len(s)) # 5
+print(s.count("l")) # 2 count the "l"
+print(s.find("l")) # find the index of "l"
+
+upperChar = s.upper()
+print(upperChar) # only return upper chapital # H
+
+answer = input("what is my name?: ")
+
+
+
