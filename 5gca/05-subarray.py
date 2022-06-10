cf662b3b2363091b04040ae6cbccf23defabd9a0
sayo mizoguchi <sl.deve080412@gmail.com>
2022-06-10 10:43:41 -0700

    practice medium problem

--- 5gca/05-subarray.py
+++ 5gca/05-subarray.py
@@ -7,10 +7,10 @@ For s = "xzxzx", the output should be solution(s) = 5.
 Consider all the ways to split s into three non-empty parts:
 * If a = "x", b = "z" and c = "xzx", then all a + b = "xz", b + c = "zxzx" and c + a = xzxx are different.
 * If a = "x", b = "zx" and c = "zx", then all a + b = "xzx", b + c = "zxzx" and c + a = zxx are different.
-* If a = "x", b = "zxz" and c = "x", then all a + b = "xzxz", b + c = "zxzx" and c + a = xx are different.
-* If a = "xz", b = "x" and c = "zx", then a + b = b + c = "xzx". Hence, this split is not counted.
-* If a = "xz", b = "xz" and c = "x", then all a + b = "xzxz", b + c = "xzx" and c + a = xxz are different.
-* If a = "xzx", b = "z" and c = "x", then all a + b = "xzxz", b + c = "zx" and c + a = xxzx are different.
-Since there are five valid ways to split s, the answer is 5.
+* If a = "x", b = "zxz" and c = "x", then all a + b = "xzxz", b + c = "zxzx" and c + a = xx are different.
+* If a = "xz", b = "x" and c = "zx", then a + b = b + c = "xzx". Hence, this split is not counted.
+* If a = "xz", b = "xz" and c = "x", then all a + b = "xzxz", b + c = "xzx" and c + a = xxz are different.
+* If a = "xzx", b = "z" and c = "x", then all a + b = "xzxz", b + c = "zx" and c + a = xxzx are different.
+Since there are five valid ways to split s, the answer is 5.
 
 '''
