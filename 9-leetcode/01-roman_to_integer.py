d53f6d4c9d3a71ac22bef7497391d20814d3b361
sayo mizoguchi <sl.deve080412@gmail.com>
2022-06-23 11:28:17 -0700

    wonderful solution and so fun

--- 9-leetcode/01-roman_to_integer.py
+++ 9-leetcode/01-roman_to_integer.py
@@ -39,7 +39,15 @@ Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 
 '''
 def solution(str):
-    pass
+    values = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+    count = 0
+
+    for i in range(len(str)):
+        if i + 1 < len(str) and values[str[i]] < values[str[i + 1]]:
+            count -= values[str[i]]
+        else:
+            count += values[str[i]]
+    return count
 
 
 
