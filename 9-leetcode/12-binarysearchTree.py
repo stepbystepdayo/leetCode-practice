3781abf1e5aee4105f9ccdd2a9987fb543743118
sayo mizoguchi <sl.deve080412@gmail.com>
2022-07-12 15:05:19 -0700

    BST

---
+++ 9-leetcode/12-binarysearchTree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        cur = root
+        count = 0
+        
+        while cur is not None:
+            if low <= cur.val >= high:
+                count += cur.val
+                
+                
+            if low > cur.val:
+                cur = cur.right
+            if high < cur.val:
+                cur = cur.left
+        return count 
+            
