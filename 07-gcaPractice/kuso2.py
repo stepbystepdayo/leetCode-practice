571094df398667f4cd590e3941c08e85d3eddfe9
sayo mizoguchi <sl.deve080412@gmail.com>
2022-07-20 11:01:20 -0700

    kuso solution

---
+++ 07-gcaPractice/kuso2.py
@@ -0,0 +1,26 @@
+
+
+def solution(arr):
+    result = []
+
+    for i in range(0,len(arr)-1,2):
+        if arr[i] > arr[i +1]:
+            result.append(arr[i + 1])
+            result.append(arr[i])
+
+        else:
+            result.append(arr[i])
+            result.append(arr[i + 1])
+    if len(arr) % 2 == 1:
+        result.append(arr[-1])
+    return result
+
+
+        
+
+
+
+
+print(solution([1,5,7,3,2,1])) #[1,5,3,7,1,2]
+print(solution([6,7,8,8,5,3,2])) #[6,7,8,8,3,5,2]
+print(solution([27])) #[27]
